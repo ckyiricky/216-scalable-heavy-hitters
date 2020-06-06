8d9a704a3b246024a2371c9ea858bb967e856500
rick <ckyiricky@gmail.com>
2020-06-05 22:55:59 -0700

    safe thread done

---
+++ common/safe_queue.cpp
@@ -0,0 +1,41 @@
+/*
+ * safe_queue.cpp
+ * Copyright (C) 2020 rick <ckyiricky@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "safe_queue.h"
+
+
+template<class T>
+void SafeQueue<T>::push(const T& data)
+{
+    std::lock_guard<std::mutex> lock(mMutex);
+    mQueue.push(data);
+    mCondition.notify_one();
+}
+template<class T>
+T SafeQueue<T>::pop()
+{
+    std::lock_guard<std::mutex> lock(mMutex);
+    mCondition.wait(lock, [&]{return !mQueue.empty();});
+}
+template<class T>
+bool SafeQueue<T>::tryPop(T& outputData)
+{
+    std::lock_guard<std::mutex> lock(mMutex);
+    if (mQueue.empty())
+    {
+        return false;
+    }
+    outputData = mQueue.front();
+    mQueue.pop();
+    return true;
+}
+template<class T>
+bool SafeQueue<T>::empty()
+{
+    std::lock_guard<std::mutex> lock(mMutex);
+    return mQueue.empty();
+}
---
+++ common/safe_queue.h
@@ -0,0 +1,34 @@
+/*
+ * safe_queue.h
+ * Copyright (C) 2020 rick <ckyiricky@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef SAFE_QUEUE_H
+#define SAFE_QUEUE_H
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+/*
+ * Thread safe queue
+ */
+template <class T>
+class SafeQueue
+{
+private:
+    std::queue<T> mQueue;
+    mutable std::mutex mMutex;
+    std::condition_variable mCondition;
+public:
+    SafeQueue(){};
+    ~SafeQueue(){};
+    void push(const T& data);
+    T pop();
+    bool tryPop(T& outputData);
+    bool empty();
+};
+
+#endif /* !SAFE_QUEUE_H */
