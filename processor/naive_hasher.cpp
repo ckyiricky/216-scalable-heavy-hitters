644104688b42dacc30aaa4186a5709cb9fe95c80
eddiehuang97 <eddiehuang97@github.com>
2020-06-12 21:23:20 -0700

    probably embarassingly wrong but whatever

--- processor/naive_hasher.cpp
+++ processor/naive_hasher.cpp
@@ -18,10 +18,31 @@ bool NaiveHasher::filter(shared_ptr<TVShows> data)
 {
     string dataId = data->getUID();
     auto hashId = mHasher->hash(dataId);
-    if (mHashTable.count(hashId) > 0) {
-        if (++mHashTable[hashId] > mThreshold)
-            return false;
-    } else 
-        mHashTable[hashId] = 1;
-    return true;
+    std::list<entry> mHashEntries = mHashTable[hashId];
+    std::list<entry> newHashEntries; // new list, not very efficient but whatever
+    std::list<entry>::iterator it;
+    bool found = false;
+    bool passed = true;
+    // find entry in hash table bucket if it exists
+    for (it = mHashEntries.begin(); it != mHashEntries.end(); ++it){
+        // might be pointer errors, I'm not sure
+        if (it->id.equals(dataId)) {
+            entry mHashEntry;
+            mHashEntry.id = it->id;
+            mHashEntry.count = it->count + 1;
+            newHashEntries.push_back(mHashEntry);
+            found = true;
+            if (mHashEntry.count > mThreshold) 
+                passed = false;
+        } else
+            newHashEntries.push_back(*it)
+    }
+    mHashTable[hashId] = newHashEntries;
+    if (!found) {
+        entry mHashEntry;
+        mHashEntry.id = dataId;
+        mHashEntry.count = 1;
+        mHashTable[hashId].push_back(mHashEntry);
+    }
+    return passed;
 }
--- processor/naive_hasher.h
+++ processor/naive_hasher.h
@@ -2,14 +2,20 @@
 #define NAIVE_HASHER_H
 
 #include <vector>
+#include <list>
 #include <unordered_map>
 #include "hasher.h"
 #include "filter.h"
 
+struct entry {
+    std::string id;
+    unsigned long count;
+}
+
 class NaiveHasher: public Filter
 {
 private:
-    std::unordered_map<unsigned long,unsigned long> mHashTable;
+    std::unordered_map<unsigned long,std::list<entry>> mHashTable;
     std::shared_ptr<Hasher> mHasher;
     unsigned long mThreshold;
 public:
